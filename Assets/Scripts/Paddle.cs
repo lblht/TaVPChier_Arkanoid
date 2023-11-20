fd1faa22a69f4f259409459394cc3bc3143f0d7c
lblht <lehota.lubomir@gmail.com>
2023-11-20 15:14:31 +0100

    wrgbk

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour
@@ -7,10 +8,25 @@ public class Paddle : MonoBehaviour
     float paddleSpeed = 60;
     float posClamp = 8f - 0.223f - 1.25f;
     float paddleWidth = 2.5f;
+    [SerializeField] bool autoPlayer = false;
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X");
+        float mouseX;
+        if(!autoPlayer)
+        {
+            mouseX = Input.GetAxis("Mouse X");
+        }
+        else
+        {
+            Transform ball = FindObjectOfType<Ball>().transform;
+            if(ball.position.x > transform.position.x + 0.4f)
+                mouseX = 0.2f;
+            else if(ball.position.x < transform.position.x - 0.4f)
+                mouseX = -0.2f;
+            else 
+                mouseX = 0;
+        }
         float amountToMove = mouseX * paddleSpeed * Time.deltaTime;
         float paddlePosX = transform.position.x + amountToMove;
         transform.position = new Vector3(Mathf.Clamp(paddlePosX, -posClamp, posClamp), transform.position.y, 0);
